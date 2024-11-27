4691107da031842456cf0a1605ca3224f9a0eb2b
cirroskais <cirroskais@madhouselabs.net>
2024-11-26 23:43:27 -0500

    abc

--- src/fastdl.ts
+++ src/fastdl.ts
@@ -15,7 +15,8 @@ function sanitize(input: string) {
         .replaceAll(":", "\\:")
         .replaceAll("[", "\\[")
         .replaceAll("]", "\\]")
-        .replaceAll("|", "\\|");
+        .replaceAll("|", "\\|")
+        .replaceAll(" ", "\\ ");
 }
 
 export async function main() {
